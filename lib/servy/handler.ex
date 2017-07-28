468fa6288191133b6816aac1910cda878965af51
Nathan Bolser <nbolser@gmail.com>
2017-07-27 21:47:20 -0600

    Add parse transformation

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -7,8 +7,12 @@ defmodule Servy.Handler do
   end
 
   def parse(request) do
-    # TODO: Parse the request string into a map:
-    conv = %{ method: "GET", path: "/vehicles", resp_body: "" }
+  	[method, path, _] =
+  		request 
+  		|> String.split( "\n") 
+  		|> List.first
+  		|> String.split(" ")
+  	%{ method: method, path: path, resp_body: "" }
   end
 
   def route(conv) do
