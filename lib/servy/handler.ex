2085c17b4e0493d90ff320f9a1cdcad2e03c6ba4
Nathan Bolser <nathan.bolser@phishme.com>
2017-07-31 13:38:33 -0600

    Add catch-all, delete route. Add status code map.

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -15,7 +15,13 @@ defmodule Servy.Handler do
   		|> String.split( "\n") 
   		|> List.first
   		|> String.split(" ")
-  	%{ method: method, path: path, resp_body: "" }
+
+  	%{
+      method: method,
+      path: path,
+      resp_body: "",
+      status: nil
+    }
   end
 
   def route(conv) do
@@ -23,16 +29,28 @@ defmodule Servy.Handler do
   end
 
   def route(conv, "GET", "/vehicles") do
-    conv = %{ conv | resp_body: "Cars, Trucks, Buses" }
+    %{ conv | status: 200, resp_body: "Cars, Trucks, Buses" }
   end
 
   def route(conv, "GET", "/cars") do
-    conv = %{ conv | resp_body: "Toyota, Jeep, Fiat" }
+    %{ conv | status: 200, resp_body: "Toyota, Jeep, Fiat" }
+  end
+
+  def route(conv, "GET", "/cars/" <> id) do
+    %{ conv | status: 200, resp_body: "Car #{id}" }
+  end
+
+  def route(conv, "DELETE", "/cars/" <> _id) do
+    %{ conv | status: 403, resp_body: "Deleting car is forbidden" }
+  end
+
+  def route(conv, _method, path) do
+    %{ conv | status: 404, resp_body: "Path #{path} not available."}
   end
 
   def format_response(conv) do
     """
-    HTTP/1.1 200 OK
+    HTTP/1.1 #{conv.status} #{status_reason(conv.status)}
     Content-Type: text/html
     Content-Length: #{String.length(conv.resp_body)}
 
@@ -40,6 +58,17 @@ defmodule Servy.Handler do
     """
   end
 
+  defp status_reason(code) do
+    %{
+      200 => "OK",
+      201 => "Created",
+      401 => "Unauthorized",
+      403 => "Forbidden",
+      404 => "Not Found",
+      500 => "Internal Server Error"
+    }[code]
+    
+  end
 end
 
 request = """
@@ -56,6 +85,30 @@ IO.puts response
 
 request = """
 GET /cars HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+response = Servy.Handler.handle(request)
+
+IO.puts response
+
+request = """
+GET /foo HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+response = Servy.Handler.handle(request)
+
+IO.puts response
+
+request = """
+GET /cars/1 HTTP/1.1
 Host: example.com
 User-Agent: ExampleBrowser/1.0
 Accept: */*
