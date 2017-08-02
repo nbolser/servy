4f864a604a7ff479ff1f6adb527ee1a5a86df8d7
Nathan Bolser <nathan.bolser@phishme.com>
2017-08-02 12:32:05 -0600

    Fix whitespacing

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -1,12 +1,12 @@
 defmodule Servy.Handler do
   def handle(request) do
-  	request
-  	|> parse
+    request
+    |> parse
     |> rewrite_path
     |> log
-  	|> route
+    |> route
     |> track
-  	|> format_response
+    |> format_response
   end
 
   def track(%{ status: 404, path: path } = conv) do
@@ -19,13 +19,13 @@ defmodule Servy.Handler do
   def log(conv), do: IO.inspect(conv)
 
   def parse(request) do
-  	[method, path, _] =
-  		request
-  		|> String.split( "\n")
-  		|> List.first
-  		|> String.split(" ")
+    [method, path, _] =
+      request
+        |> String.split( "\n")
+        |> List.first
+        |> String.split(" ")
 
-  	%{
+    %{
       method: method,
       path: path,
       resp_body: "",
